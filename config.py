eb107aefba27b4d5e7d34928bad1bae59c7b5b2a
sahithyaravi1493 <sahithyaravi1493@gmail.com>
2021-07-13 15:48:18 +0200

    config

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 DOWNLOAD_DATASET_AGAIN = False
 
 # Pre-Processing dataset
-PROCESS_DATASET = True
-PROCESSING_PARTS_OF_SPEECH_INCLUDED = ['NOUN']
+PROCESS_DATASET = False
+PROCESSING_PARTS_OF_SPEECH_INCLUDED = ['NOUN', 'VERB']
 
 # Model Training
-GRID_SEARCH = True  # Grid Search for best Model for this setting.
-FILTER_WORDS = 0.8  # 0.8 means filter words repeated in more than 80% of documents
+GRID_SEARCH = False  # Grid Search for best Model for this setting.
+FILTER_WORDS = 0.6  # 0.8 means filter words repeated in more than 80% of documents
 FINAL_MODEL = True  # Generate final model with best params from grid search
 
